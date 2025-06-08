1ec9bc768509c7751bc2cd32ecfa03b87fc343aa
Zivotu <130804369+Zivotu@users.noreply.github.com>
2025-06-09 00:22:30 +0200

    fix language selector

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,6 +9,8 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen bg-white flex flex-col">
       <Navigation />
+      {/* Hidden container required for Google Translate */}
+      <div id="google_translate_element" className="hidden" />
       <main className="flex-1 pt-16">
         {children}
       </main>
