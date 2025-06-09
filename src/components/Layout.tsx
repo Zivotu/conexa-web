391f74c276463e7d55d61985b08d9345aa122723
Zivotu <130804369+Zivotu@users.noreply.github.com>
2025-06-09 03:46:18 +0200

    Offset navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,35 +1,17 @@
-import { useEffect, useState } from 'react';
 import Navigation from './Navigation';
 import Footer from './Footer';
+
+const TOOLBAR_OFFSET = 50;
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const [toolbarHeight, setToolbarHeight] = useState(0);
-
-  useEffect(() => {
-    const update = () => {
-      const banner = document.querySelector<HTMLIFrameElement>('.goog-te-banner-frame');
-      const height = banner ? banner.getBoundingClientRect().height : 0;
-      setToolbarHeight(height);
-    };
-
-    update();
-    const observer = new MutationObserver(update);
-    observer.observe(document.body, { childList: true, subtree: true });
-    window.addEventListener('resize', update);
-    return () => {
-      observer.disconnect();
-      window.removeEventListener('resize', update);
-    };
-  }, []);
-
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <Navigation offset={toolbarHeight} />
-      <main className="flex-1" style={{ paddingTop: 64 + toolbarHeight }}>
+      <Navigation offset={TOOLBAR_OFFSET} />
+      <main className="flex-1" style={{ paddingTop: 64 + TOOLBAR_OFFSET }}>
         {children}
       </main>
       <Footer />
