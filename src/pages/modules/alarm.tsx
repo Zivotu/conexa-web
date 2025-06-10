ef3c0d7e7ed5239024b5fc945cd38ba5c44980ed
Zivotu <130804369+Zivotu@users.noreply.github.com>
2025-06-10 17:41:17 +0200

    i18n: translate pages

--- src/pages/modules/alarm.tsx
+++ src/pages/modules/alarm.tsx
@@ -1,17 +1,21 @@
 import Layout from '@/components/Layout';
 import ModuleNav from '@/components/ModuleNav';
+import { useTranslation } from 'react-i18next';
 
-const Alarm: React.FC = () => (
-  <Layout>
-    <ModuleNav currentId="alarm" />
-    <section className="py-20 bg-white">
-      <div className="container mx-auto px-4 text-center">
-        <h1 className="font-poppins font-semibold text-4xl text-gray-900">
-          This module is still under construction with help from our users' feedback. Coming soon!
-        </h1>
-      </div>
-    </section>
-  </Layout>
-);
+const Alarm: React.FC = () => {
+  const { t } = useTranslation();
+  return (
+    <Layout>
+      <ModuleNav currentId="alarm" />
+      <section className="py-20 bg-white">
+        <div className="container mx-auto px-4 text-center">
+          <h1 className="font-poppins font-semibold text-4xl text-gray-900">
+            {t('module_alarm.coming_soon')}
+          </h1>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
 export default Alarm;
